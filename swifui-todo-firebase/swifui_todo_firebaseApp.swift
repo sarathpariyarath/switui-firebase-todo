0e6c3971550cebf8fd00668539510f82b0141a82
sarathpariyarath <sarath.pariyarath@gmail.com>
2022-01-24 13:18:58 +0530

    Initial Commit

---
+++ swifui-todo-firebase/swifui_todo_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifui_todo_firebaseApp.swift
+//  swifui-todo-firebase
+//
+//  Created by Sarath P on 24/01/22.
+//
+
+import SwiftUI
+
+@main
+struct swifui_todo_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
